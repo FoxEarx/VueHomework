063244248b97dfe029190a5941e777d39f92f021
ForEars <1228456740@qq.com>
2022-06-30 19:25:26 +0800

    移动端-导航切换效果完成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,174 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="wrap">
+    <div class="nav_left" id="navLeft">
+      <div class="nav_content">
+        <span
+          :class="{ active: item.first_id === gl }"
+          v-for="item in arr"
+          :key="item.first_id"
+          @click="idFn(item.first_id)"
+          >{{ item.first_name }}</span
+        >
+      </div>
+    </div>
+    <div class="down">
+      <i class="iconfont icon-xiajiantoubeifen gray"></i>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      arr: [
+        {
+          first_id: '0',
+          first_name: '热门',
+        },
+        {
+          first_id: '621',
+          first_name: '\u5496\u5561',
+        },
+        {
+          first_id: '627',
+          first_name: '\u996e\u98df',
+        },
+        {
+          first_id: '279',
+          first_name: '\u7537\u88c5',
+        },
+        {
+          first_id: '294',
+          first_name: '\u5973\u88c5',
+        },
+        {
+          first_id: '122',
+          first_name: '\u773c\u955c',
+        },
+        {
+          first_id: '339',
+          first_name: '\u5185\u8863\u914d\u9970',
+        },
+        {
+          first_id: '391',
+          first_name: '\u6bcd\u5a74',
+        },
+        {
+          first_id: '35',
+          first_name: '\u978b\u9774',
+        },
+        {
+          first_id: '39',
+          first_name: '\u8fd0\u52a8',
+        },
+        {
+          first_id: '153',
+          first_name: '\u7bb1\u5305',
+        },
+        {
+          first_id: '119',
+          first_name: '\u7f8e\u5986\u4e2a\u62a4',
+        },
+        {
+          first_id: '355',
+          first_name: '\u5bb6\u7eba',
+        },
+        {
+          first_id: '51',
+          first_name: '\u9910\u53a8',
+        },
+        {
+          first_id: '334',
+          first_name: '\u7535\u5668',
+        },
+        {
+          first_id: '369',
+          first_name: '\u5bb6\u88c5',
+        },
+        {
+          first_id: '10',
+          first_name: '\u5bb6\u5177',
+        },
+        {
+          first_id: '223',
+          first_name: '\u6570\u7801',
+        },
+        {
+          first_id: '429',
+          first_name: '\u6c7d\u914d',
+        },
+        {
+          first_id: '546',
+          first_name: '\u5065\u5eb7\u4fdd\u5065',
+        },
+        {
+          first_id: '433',
+          first_name: '\u5b9a\u5236',
+        },
+      ],
+      gl: '0',
+    }
+  },
+  methods: {
+    idFn(id) {
+      // const index = this.arr.findIndex((ele) => id == ele.id)
+      this.gl = id
+    },
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.wrap {
+  width: 100%;
+  display: flex;
+  margin: 0.2rem 0 0 0;
+  position: relative;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+/*左侧的导航样式*/
+.nav_left {
+  width: 21.1875rem;
+  overflow: scroll;
 }
-li {
+
+.nav_left::-webkit-scrollbar {
+  display: none;
+}
+
+.nav_content {
+  white-space: nowrap;
+  padding: 0 0.7rem;
+}
+
+.nav_content span {
   display: inline-block;
-  margin: 0 10px;
+  padding: 0.4rem 0.6rem;
+  font-size: 0.875rem;
 }
-a {
-  color: #42b983;
+
+.nav_content .active {
+  border-bottom: 2px solid #7f4395;
+  color: #7f4395;
+}
+
+.nav_left,
+.down {
+  float: left;
+}
+
+/*右侧导航部分*/
+.down {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.gray {
+  color: gray;
+  display: inline-block;
+  vertical-align: middle;
 }
 </style>
