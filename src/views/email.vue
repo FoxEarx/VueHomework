9f72700ac897c91d055a5e0cf5c6ad560f4a3b66
ForEars <1228456740@qq.com>
2022-07-06 20:09:37 +0800

    完善

--- src/views/email.vue
+++ src/views/email.vue
@@ -2,14 +2,7 @@
   <div>
     <div class="diqu"><p>选择所在地区（实名认证主题所在地）</p></div>
     <div class="w">
-      <input type="radio" name="city" id="" />中国大陆
-      <input type="radio" name="city" id="" />中国香港
-      <p></p>
-      <div class="tem">
-        <input type="email" name="" id="" placeholder="请输邮箱" />
-      </div>
-      <input type="email" name="" id="" placeholder="请输入验证码" />
-      <button>发送验证码</button>
+      <slot name="email"></slot>
     </div>
   </div>
 </template>
