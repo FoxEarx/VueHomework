a3d43413bad765ea8ac1b61dbba2b7aded28bd99
ForEars <1228456740@qq.com>
2022-07-06 21:27:44 +0800

    半成品

---
+++ src/components/list.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <Listzdy :obj="list">
+      <template v-slot:id></template>
+      <template v-slot:name></template>
+      <template v-slot:age></template>
+      <template v-slot:img></template>
+    </Listzdy>
+  </div>
+</template>
+
+<script>
+import Listzdy from './listzdy.vue'
+export default {
+  components: { Listzdy },
+  data() {
+    return {
+      list: [
+        {
+          id: 100,
+          name: '小传同学',
+          age: 18,
+          headImgUrl:
+            'http://yun.itheima.com/Upload/./Images/20210303/603f2d2153241.jpg',
+          type: 0, // 0 显示成a标签
+        },
+        {
+          id: 101,
+          name: '小黑同学',
+          age: 25,
+          headImgUrl:
+            'http://yun.itheima.com/Upload/./Images/20210304/6040b101a18ef.jpg',
+          type: 1, // 1 显示成图片
+        },
+        {
+          id: 102,
+          name: '智慧同学',
+          age: 21,
+          headImgUrl:
+            'http://yun.itheima.com/Upload/./Images/20210302/603e0142e535f.jpg',
+          type: 1, // 1 显示成图片
+        },
+        {
+          id: 103,
+          name: '智慧同学',
+          age: 21,
+          headImgUrl:
+            'http://yun.itheima.com/Upload/./Images/20210302/603e0142e535f.jpg',
+          type: 2, // 2 显示成文本
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ src/components/listzdy.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <th>
+          <td>序号</td>
+          <td>姓名</td>
+          <td>年龄</td>
+          <td>图片</td>
+        </th>
+      </thead>
+      <tbody>
+        <tr v-for="item in obj" :key="item.id">
+          <slot name="id">{{item.id}}</slot>
+          <slot name="name">{{item.name}}</slot>
+          <slot name="age">{{item.age}}</slot>
+          <slot name="img" v-if="item.type==0"><a :href="item.headImgUrl"></a></slot>
+         
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+props:{
+    obj:{
+        type:Array,
+        default:()=>[]
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
